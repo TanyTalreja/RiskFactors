eec402f727cdd26f93a5b7254ffba12ac5f58e89
Tanishq Talreja <tanishqtalreja19@gmail.com>
2023-04-27 10:45:08 -0400

    updated

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -21,7 +21,7 @@ const Navbar = () =>
             className="text-md font-bold leading-relaxed inline-block mr-4 ml-2 py-2 whitespace-nowrap uppercase text-white"
             href="#pablo"
           >
-            Risk Visualization
+            Risk Visualization by Tanishq Talreja
               </a>
           <button
             className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
